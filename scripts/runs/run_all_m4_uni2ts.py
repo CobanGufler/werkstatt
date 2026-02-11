99bedfcc5d5a8e02f2065eec0815d7e9797b59c8
guma <m.gufler2004@gmail.com>
2026-02-11 17:56:38 +0100

    auskommentierten teil löschen

--- scripts/runs/run_all_m4_uni2ts.py
+++ scripts/runs/run_all_m4_uni2ts.py
@@ -293,65 +293,6 @@ def evaluate_moirai_m4_uni2ts(
 
 
 if __name__ == "__main__":
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("--group", type=str, required=True)
-    # parser.add_argument("--data_dir", type=str, required=True)
-    # parser.add_argument("--save_dir", type=str, default="results")
-    # parser.add_argument("--run_name", type=str, default="test")
-    #
-    # parser.add_argument("--timesfm_ckpt", type=str, default="google/timesfm-1.0-200m-pytorch")
-    # parser.add_argument("--chronos_ckpt", type=str, default="amazon/chronos-t5-tiny")
-    # parser.add_argument("--moirai_repo", type=str, default="Salesforce/moirai-1.0-R-small")
-    #
-    # parser.add_argument("--context_len", type=int, default=512)
-    # parser.add_argument("--batch_size", type=int, default=256)
-    # parser.add_argument("--chronos_samples", type=int, default=10)
-    # parser.add_argument("--moirai_samples", type=int, default=10)
-    # parser.add_argument("--moirai_batch", type=int, default=256)
-    #
-    # args = parser.parse_args()
-    #
-    # out_dir = os.path.join(args.save_dir, args.run_name)
-    # os.makedirs(out_dir, exist_ok=True)
-    #
-    # df1 = evaluate_timesfm_m4_uni2ts(
-    #     group=args.group,
-    #     data_dir=args.data_dir,
-    #     checkpoint=args.timesfm_ckpt,
-    #     save_path=os.path.join(out_dir, f"timesfm_m4_{args.group}_uni2ts.csv"),
-    #     context_len=args.context_len,
-    #     batch_size=args.batch_size,
-    # )
-    #
-    # df2 = evaluate_chronos_m4_uni2ts(
-    #     group=args.group,
-    #     data_dir=args.data_dir,
-    #     checkpoint=args.chronos_ckpt,
-    #     save_path=os.path.join(out_dir, f"chronos_m4_{args.group}_uni2ts.csv"),
-    #     context_len=args.context_len,
-    #     num_samples=args.chronos_samples,
-    #     batch_size=args.batch_size,
-    # )
-    #
-    # df3 = evaluate_moirai_m4_uni2ts(
-    #     group=args.group,
-    #     data_dir=args.data_dir,
-    #     repo_id=args.moirai_repo,
-    #     save_path=os.path.join(out_dir, f"moirai_m4_{args.group}_uni2ts.csv"),
-    #     context_len=args.context_len,
-    #     num_samples=args.moirai_samples,
-    #     batch_size=args.moirai_batch,
-    # )
-    #
-    # merged = pd.concat([df1, df2, df3], axis=0)
-    # merged.index = [
-    #     f"timesfm_m4_{args.group}_uni2ts",
-    #     f"chronos_m4_{args.group}_uni2ts",
-    #     f"moirai_m4_{args.group}_uni2ts",
-    # ]
-    # merged.to_csv(os.path.join(out_dir, f"ALL_m4_{args.group}_uni2ts.csv"))
-    # print(merged)
-
     parser = argparse.ArgumentParser()
     parser.add_argument("--group", type=str, required=True)
     parser.add_argument("--data_dir", type=str, required=True)
@@ -359,8 +300,8 @@ if __name__ == "__main__":
     parser.add_argument("--run_name", type=str, default="test")
 
     parser.add_argument("--timesfm_ckpt", type=str, default="google/timesfm-1.0-200m-pytorch")
-    parser.add_argument("--chronos_ckpt", type=str, default="amazon/chronos-t5-base")
-    parser.add_argument("--moirai_repo", type=str, default="Salesforce/moirai-1.0-R-base")
+    parser.add_argument("--chronos_ckpt", type=str, default="amazon/chronos-t5-base") #"amazon/chronos-t5-tiny"
+    parser.add_argument("--moirai_repo", type=str, default="Salesforce/moirai-1.0-R-base") #"Salesforce/moirai-1.0-R-small"
 
     parser.add_argument("--context_len", type=int, default=512)
     parser.add_argument("--batch_size", type=int, default=64)
